99cf2e6de2104eae4fdf15535ac7d12ccda693aa
Girish N <n_girish@hotmail.com>
2025-08-31 18:46:57 +1000

    Refactor S3Stack to use S3Config for bucket name and jar file paths

---
+++ infra/config.py
@@ -0,0 +1,4 @@
+class S3Config:
+    S3_BUCKET_NAME = "gn-flink-bucket"
+    JAR_FILE_PATH = "../../artifacts/KinesisS3Sink-1.0-SNAPSHOT.jar"
+    JAR_DESTINATION_KEY = "KinesisS3Sink-1.0-SNAPSHOT.jar"
--- infra/stacks/s3_stack.py
+++ infra/stacks/s3_stack.py
@@ -1,9 +1,13 @@
+import os
+from config import S3Config
+
 from aws_cdk import (
     Stack,
-    Duration,
-    aws_s3 as s3
+    aws_s3 as s3,
+    aws_s3_deployment as s3deploy,
 )
 from constructs import Construct
+
 
 class S3Stack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs):
@@ -12,5 +16,13 @@ class S3Stack(Stack):
         self.s3 = s3.Bucket(
             self,
             "FlinkS3Bucket",
-            bucket_name="gn-flink-bucket",
+            bucket_name=S3Config.S3_BUCKET_NAME,
+        )
+
+        file_path = os.path.join(os.path.dirname(__file__), S3Config.JAR_FILE_PATH)
+
+        # Deploy the jar file to the S3 bucket
+        s3deploy.BucketDeployment(self, "DeployJarFile",
+            sources=[s3deploy.Source.asset(file_path)],
+            destination_bucket=self.s3
         )
